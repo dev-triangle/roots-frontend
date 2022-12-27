6646a5825864d64d501af6c72252d932a9d1652a
Lekshmi S <slekshmi3949@gmail.com>
2022-12-27 11:42:37 +0530

    initial commit

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -4,7 +4,12 @@ import MainLayout from '../../components/MainLayout/MainLayout'
 const Homepage = () => {
   return (
     <MainLayout>
-      Landing Page here
+    <div className='home_container'>
+     <div className='home_title'>ROOTS</div>
+      <div className='home_intro'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nunc, tortor mauris eget ut integer sit. Purus consequat urna vulputate turpis blandit sed.</div>
+       <div className='home_button'><span className='h_button'>EXPLORE</span></div>
+      
+    </div>
     </MainLayout>
   )
 }
