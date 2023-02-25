91dd88b44bfc3aaef8b1469918a16014c1454d87
Anuj Kumar Sharma <niranjanamor@gmail.com>
2023-02-25 14:30:43 +0530

    guide img added

--- src/pages/BecomeGuidePg/BecomGuidePg.js
+++ src/pages/BecomeGuidePg/BecomGuidePg.js
@@ -18,7 +18,7 @@ const BecomGuidePg = () => {
   },[])
   return (
     <MainLayout>
-      <div className="guide__page__main_container">
+      {/* <div className="guide__page__main_container">
         <img src="" alt="" className="guide__vector" />
         <div className="form__part__guide">
           <FormControl fullWidth>
@@ -42,7 +42,34 @@ const BecomGuidePg = () => {
           </FormControl>
           <BecomeGuideForm  placeId={placeId}/>
         </div>
+      </div> */}
+
+<div className='guide_main'>
+      
+      <div className='guide_img'></div>
+
+      <div className='g_container'>
+            <p className='g_heading'>Book With Us Today</p>
+
+            <div className='g_dot1'></div> <p className='gl_dot1'>One way</p>
+            <div className='g_dot2'></div><p className='gl_dot2'>Round trip</p>
+
+            <input className='gu_input1' type='text'  placeholder='Leaving from...' required/>
+            <input className='gu_input2' type="text"   placeholder='Going to...' required/>
+
+            <input className='gu_input3' type="text" onChange={(e) => console.log(e.target.value)}
+              onFocus={(e) => (e.target.type = "date")}
+              onBlur={(e) => (e.target.type = "text")} placeholder='Depart' required/>
+            <input className='gu_input4' type="text" onChange={(e) => console.log(e.target.value)}
+              onFocus={(e) => (e.target.type = "date")}
+              onBlur={(e) => (e.target.type = "text")}   placeholder='Return' required/>
+
+            <input className='gu_input5' type="number"   placeholder='Passengers' required/>
+            <input className='gu_input6' type="text"   placeholder='Class' required/>
+          
+            <div className='g_button' type='submit'>Search Available Flights</div> 
       </div>
+   </div>
     </MainLayout>
   );
 };
