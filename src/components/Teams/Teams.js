421636c9c8335ddf6df749f7793a8d8079bba969
jacobsanosh <jacobsanosh@gmail.com>
2023-03-08 23:16:18 +0530

    added

--- src/components/Teams/Teams.js
+++ src/components/Teams/Teams.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Teams.css'
+
 const Teams = () => {
   return (
-    <div>
-        <h1>Teams</h1>
+    <div className='team__container'>
+
+        <h1 className='team__header'>Teams</h1>
     </div>
   )
 }
