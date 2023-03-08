5928737a629c8c390c0315ccc4b6d17ecff196b0
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-03-08 23:47:44 +0530

    staging

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,51 +1,82 @@
-import React, { useState } from 'react'
-import './Login.css'
-import MainLayout from '../../components/MainLayout/MainLayout'
-import { baseUrl } from '../../utils/urls'
-import axiosInstance from '../../auth/authHandler'
-import { useNavigate } from 'react-router-dom'
-import CustomTitle from '../../utils/CustomTitle'
+import React, { useState } from "react";
+import "./Login.css";
+import MainLayout from "../../components/MainLayout/MainLayout";
+import { baseUrl } from "../../utils/urls";
+import axiosInstance from "../../auth/authHandler";
+import { useNavigate } from "react-router-dom";
+import CustomTitle from "../../utils/CustomTitle";
+import Lottie from "react-lottie";
+import login_anime from "../../assets/animations/login_anime.json";
 const Login = () => {
-  const navigate=useNavigate();
-  const[email,setEmail]=useState("")
-  const[password,setPassword]=useState("")
-  const handleLogin= async(e)=>{
-    e.preventDefault()
-    axiosInstance.post(`${baseUrl}/api/token/`,{
-        "email": email,
-        "password" : password
-    }).then((res)=>{
-        // console.log(res)
-        localStorage.setItem('access_token',res.data.access);
-        localStorage.setItem('refresh_token',res.data.refresh);
-        axiosInstance.defaults.headers['Authorization']= 'Bearer ' + localStorage.getItem('access_token');
-        if(res.status===200)
-        
-        navigate('/')
-    },(error)=>{
-        
-    })
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    axiosInstance
+      .post(`${baseUrl}/api/token/`, {
+        email: email,
+        password: password,
+      })
+      .then(
+        (res) => {
+          // console.log(res)
+          localStorage.setItem("access_token", res.data.access);
+          localStorage.setItem("refresh_token", res.data.refresh);
+          axiosInstance.defaults.headers["Authorization"] =
+            "Bearer " + localStorage.getItem("access_token");
+          if (res.status === 200) navigate("/");
+        },
+        (error) => {}
+      );
+  };
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: login_anime,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
-}
   return (
     <MainLayout>
-      <CustomTitle title="Login"/>
-      <div className='su__main'>
+      <CustomTitle title="Login" />
+      <div className="su__main">
         <div className="su__blank_space">
+        <Lottie options={defaultOptions} height={400} width={400} />
+
         </div>
         <div className="su__form_container">
-          <p className='su__welcome_text'>Welcome back to Roots</p>
-          <p className='su__and_continue'>Login</p>
-          <form className='su__main_form' onSubmit={handleLogin}>
-            <input value={email} className='su__input_taker' placeholder='email' type="text" onChange={(e) => { setEmail(e.target.value) }} />
-            <input value={password} className='su__input_taker' placeholder='password' type="password" onChange={(e) => { setPassword(e.target.value) }} />
-            <button type='submit' className='su__submit_button'>Login</button>
+          <p className="su__welcome_text">Welcome back to Roots</p>
+          <p className="su__and_continue">Login</p>
+          <form className="su__main_form" onSubmit={handleLogin}>
+            <input
+              value={email}
+              className="su__input_taker"
+              placeholder="email"
+              type="text"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <input
+              value={password}
+              className="su__input_taker"
+              placeholder="password"
+              type="password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <button type="submit" className="su__submit_button">
+              Login
+            </button>
           </form>
         </div>
       </div>
     </MainLayout>
+  );
+};
 
-  )
-}
-
-export default Login
+export default Login;
--- src/pages/ProfilePg/PurchaseData.js
+++ src/pages/ProfilePg/PurchaseData.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axiosInstance from "../../auth/authHandler";
+import { baseUrl } from "../../utils/urls";
 import "./ProfilePg.css";
 const PurchaseData = () => {
+  const [purchases, setPurchases] = useState([]);
+  const [userId, setUserId] = useState(0);
+  useEffect(() => {
+    axiosInstance.get(`${baseUrl}/current-user/`).then((response) => {
+      setUserId(response.data.id);
+    });
+  });
+  useEffect(() => {
+    axiosInstance
+      .get(`${baseUrl}/purchases/`)
+      .then((response) => setPurchases(response.data));
+  }, []);
   return (
-    <div>
+    <div className="purchase_section_container">
       <h1 className="profile__pg_title">Purchases</h1>
+      <div className="registered__card">
+        {purchases.map((purchase, index) =>
+          purchase.user_foreign === parseInt(userId) ? (
+            <div className="reg__cardbody" key={index}>
+              <div className="reg__booked_content">
+                <p className="reg__content_p">
+                  Purchase ID: <span>{purchase.id}</span>
+                </p>
+                <p className="reg__content_p">
+                  Date: <span>{purchase.date_of_purchase}</span>
+                </p>
+              </div>
+            </div>
+          ) : null
+        )}
+      </div>
     </div>
   );
 };
