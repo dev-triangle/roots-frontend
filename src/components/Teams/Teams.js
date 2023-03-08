c267edbb6095f1af8fa660fa106db2ecec10bd6a
jacobsanosh <jacobsanosh@gmail.com>
2023-03-08 23:43:56 +0530

    adding team

--- src/components/Teams/Teams.js
+++ src/components/Teams/Teams.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Teams.css'
-
+import { peoples } from '../../data/team'
+import TeamCard from './TeamCard'
 const Teams = () => {
   return (
     <div className='team__container'>
-
         <h1 className='team__header'>Teams</h1>
+        <TeamCard/>
     </div>
   )
 }
