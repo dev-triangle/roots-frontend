b6214a845f7e408d41ab278e808c81440fa419c3
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-03-08 22:58:48 +0530

    added team data

---
+++ src/assets/jpg/index.js
@@ -0,0 +1,4 @@
+export { default as navya } from "./navya.jpg";
+export { default as sanosh } from "./sanosh.jpg";
+export { default as niranjana } from "./niranjana.jpg";
+export { default as achyuth } from "./achyuth.jpg";
---
+++ src/data/team.js
@@ -0,0 +1,28 @@
+import { niranjana, navya, sanosh, achyuth } from "../assets/jpg/index";
+export const peoples = [
+  {
+    id: 1,
+    name: "Sanosh Jacob",
+    image: sanosh,
+  },
+  {
+    id: 2,
+    name: "Achyuth Mohan",
+    image: achyuth,
+  },
+  {
+    id: 3,
+    name: "Lekshmi S",
+    image: "https://www.w3schools.com/howto/img_avatar.png",
+  },
+  {
+    id: 4,
+    name: "Niranjana B Nair",
+    image: niranjana,
+  },
+  {
+    id: 5,
+    name: "Navya PS",
+    image: navya,
+  },
+];
